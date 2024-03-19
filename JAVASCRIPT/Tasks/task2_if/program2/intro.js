9603a128b55dcb22e883edb568f89db64837146d
VARNIRAJ SILICA <thummarvrunda51@gmail.com>
2024-03-19 17:30:01 +0530

    all commit

---
+++ JAVASCRIPT/Tasks/task2_if/program2/intro.js
@@ -0,0 +1,13 @@
+// Check if a person is eligible to vote
+
+let age = 60
+
+if (age >= 18) 
+{
+    console.log("Eligible for vote.");
+}
+else 
+{
+    console.log("Not eligible for vote.");
+}
+
---
+++ JAVASCRIPT/Tasks/task2_if/program3/intro.js
@@ -0,0 +1,32 @@
+// Determine the largest of three numbers
+
+let v = 10
+let a = 40
+let b = 30
+
+if (v >= a) {
+
+    if (v >= b) {
+
+        console.log(v+" is Largest Value.");       
+
+    }
+    else {
+
+        console.log(b+" is Largest Value.");
+
+    }
+
+}
+
+else if (a >= b) {
+
+    console.log(a+" is Largest Value.");
+
+}
+
+else {
+
+    console.log(b+" is Largest Value.");
+
+}
